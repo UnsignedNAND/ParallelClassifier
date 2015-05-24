f903418a6dd047612c2241742d09fd716ce99233
Maciej Szankin <maciej@szankin.pl>
2015-05-25 00:04:39 +0200

    Added texmaker to dependencies.
    Signed-off-by: Maciej Szankin <maciej@szankin.pl>

--- doc/install-dependencies.sh
+++ doc/install-dependencies.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 sudo apt-get install liblatex-driver-perl texlive-latex-recommended texlive-fonts-recommended texlive-latex-extra
+
+# Install visual editor
+sudo apt-get install texmaker
