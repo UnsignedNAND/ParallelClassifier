480cf6e2abcb43617b100adf1e27dee886530e80
Maciej Szankin <maciej@szankin.pl>
2015-05-25 22:44:52 +0200

    Tuned FIO benchmark script.
    Signed-off-by: Maciej Szankin <maciej@szankin.pl>

--- benchmarks/fio.sh
+++ benchmarks/fio.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
- fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=randrw --filename=test.fio --bs=4k --iodepth=64 --size=1G --readwrite=randrw --rwmixread=75
- rm test.fio
+x=0
+while [ $x -le 10 ] ; do
+    fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=randrw --filename=test.fio --bs=4k --iodepth=64 --size=1G --readwrite=randrw --rwmixread=75
+    rm test.fio
+    x=$[x + 1]
+done
