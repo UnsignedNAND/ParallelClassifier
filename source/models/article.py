d29029447584b394ad537a1dfc5cd1c5d54251c9
Maciej Szankin <maciej@szankin.pl>
2015-10-27 21:11:12 +0100

    getting title and text from xml

--- source/models/article.py
+++ source/models/article.py
@@ -5,3 +5,14 @@ class Article(object):
 
     def __init__(self):
         pass
+
+    def print_short(self):
+        self.remove_chars()
+        print '%s : %s : %s' % (self.wiki_category, self.title, self.text[:80].replace('\n', ''))
+
+    def remove_chars(self):
+        chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '|', '\\'
+                 '/', '?', '>', '<', ',', '.']
+
+        for char in chars:
+            self.text = self.text.replace(char, ' ')
--- source/reader.py
+++ source/reader.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as etree
 import os
+from models.article import Article
 
 
 class Reader(object):
@@ -16,25 +17,32 @@ class Reader(object):
 
     def parse_xml(self):
         articles_parsed = 0
+        article_title = None
+        article_category = None
+        article_text = None
+
         for event, elem in etree.iterparse(self.xml_path, events=('start', 'end', 'start-ns', 'end-ns')):
             if event == 'start' or str(elem).find('text') == -1:
                 if event != 'start' and str(elem).find('title') != -1:
                     article_title = unicode(elem.text).encode('utf8')
-                    print article_title
             else:
-                current_category = "Unknown"
+                article_category = 'Unknown'
                 try:
-                    current_category = unicode(elem.text).encode('utf8').split("Category:")[1]
-                    current_category = current_category.split('|')[0]
+                    article_category = unicode(elem.text).encode('utf8').split("Category:")[1]
+                    article_category = article_category.split('|')[0]
                 except Exception as ex:
                     pass
-                # print current_category
-                # print unicode(elem.text).encode('utf8')
-                # print "*" * 100
+                article_text = unicode(elem.text).encode('utf8')
+
+                article = Article()
+                article.title = article_title
+                article.wiki_category = article_category
+                article.text = article_text
+                article.print_short()
                 articles_parsed += 1
-                break
-                # if articles_parsed % 1000 == 0:
-                #     print articles_parsed
+
+                if articles_parsed > 5:
+                    break
 
 
 if __name__ == '__main__':
