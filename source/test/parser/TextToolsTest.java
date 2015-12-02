03501e072fc287af5b5b892f432f81d4be1ea384
Maciej Szankin <maciej@szankin.pl>
2015-12-02 21:43:19 +0100

    Removed deprecated tests.

--- source/test/parser/TextToolsTest.java
+++ source/test/parser/TextToolsTest.java
@@ -35,46 +35,5 @@ public class TextToolsTest {
     
     @After
     public void tearDown() {
-    }
-
-    /**
-     * Test of Stemm method, of class TextTools.
-     */
-    @Test
-    public void testStemm() {
-        System.out.println("Stemm");
-        String str = "";
-        String expResult = "";
-        String result = TextTools.Stemm(str);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of TrimWhiteSpaces method, of class TextTools.
-     */
-    @Test
-    public void testTrimWhiteSpaces() {
-        System.out.println("TrimWhiteSpaces");
-        String str = "";
-        String expResult = "";
-        String result = TextTools.TrimWhiteSpaces(str);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of LowerString method, of class TextTools.
-     */
-    @Test
-    public void testLowerString() {
-        System.out.println("LowerString");
-        String str = "AbC DeF";
-        String expResult = "abc def";
-        String result = TextTools.LowerString(str);
-        assertEquals(expResult, result);
-    }
-    
+    } 
 }
