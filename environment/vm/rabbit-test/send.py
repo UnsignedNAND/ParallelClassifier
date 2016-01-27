0c74f16f9db4ef8798f7f5de2dc3e67196767712
Maciej Szankin <maciej@szankin.pl>
2016-01-27 18:55:57 +0100

    Added multinode vagrant testbed with communication via RabbitMQ

--- environment/vm/rabbit-test/send.py
+++ environment/vm/rabbit-test/send.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
+from datetime import datetime
 import pika
+import socket
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+        host='localhost', port=5672))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
 channel.basic_publish(exchange='',
                       routing_key='hello',
-                      body='Hello World!')
+                      body='Hello World! From: ' + socket.gethostname() + ' @ ' + str(datetime.now()))
 print(" [x] Sent 'Hello World!'")
 connection.close()
