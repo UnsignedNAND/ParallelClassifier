b0ffd906c86f9e7e1d09a77df1015c219898e209
Maciej Szankin <maciej@szankin.pl>
2016-02-29 20:37:20 +0100

    added BOW

--- source/compute/process_text.py
+++ source/compute/process_text.py
@@ -13,6 +13,16 @@ def _trim_white_spaces(text):
     return " ".join(text.split())
 
 
+def _bag_of_words(text):
+    bow = {}
+    for word in text.split():
+        if word in bow.keys():
+            bow[word] += 1
+        else:
+            bow[word] = 1
+    return bow
+
+
 def simplify(text):
     text = _lower_string(text)
     text = _remove_special_chars(text)
