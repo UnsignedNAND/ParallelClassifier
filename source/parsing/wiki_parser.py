e0226d4f82c895ed520b615c699133c754dc9acc
Maciej Szankin <maciej.szankin@intel.com>
2016-06-16 00:53:28 -0500

    added pipe for IDF

--- source/parsing/wiki_parser.py
+++ source/parsing/wiki_parser.py
@@ -11,79 +11,108 @@ CONF = get_conf()
 LOG = get_log()
 
 
-class ProcessReader(multiprocessing.Process):
-    def __init__(self, q_unparsed_documents):
-        self._q_unparsed_documents = q_unparsed_documents
-        super(self.__class__, self).__init__()
+class Process(object):
+    class Reader(multiprocessing.Process):
+        def __init__(self, q_unparsed_documents):
+            self._q_unparsed_documents = q_unparsed_documents
+            super(self.__class__, self).__init__()
 
-    def run(self):
-        wiki_handler = WikiContentHandler(self._q_unparsed_documents)
-        sax_parser = xml.sax.make_parser()
-        sax_parser.setContentHandler(wiki_handler)
+        def run(self):
+            wiki_handler = WikiContentHandler(self._q_unparsed_documents)
+            sax_parser = xml.sax.make_parser()
+            sax_parser.setContentHandler(wiki_handler)
 
-        try:
-            data_source = open('../data/wiki_dump.xml')
-            sax_parser.parse(data_source)
-            LOG.info('Parsed {0} items'.format(wiki_handler.items_saved))
-        except PageLimitException as page_limit_exception:
-            LOG.info(page_limit_exception)
-        except KeyboardInterrupt:
-            exit()
-        finally:
-            # A pill for other threads
-            self._q_unparsed_documents.put(None)
-
-
-class ProcessParser(multiprocessing.Process):
-    def __init__(self, q_unparsed_documents):
-        self._q_unparsed_documents = q_unparsed_documents
-        super(self.__class__, self).__init__()
-
-    def run(self):
-        c = 0
-        while True:
-            page = self._q_unparsed_documents.get()
-            if page is None:
-                # Just to be sure that other threads can also take a pill
+            try:
+                data_source = open('../data/wiki_dump.xml')
+                sax_parser.parse(data_source)
+                LOG.info('Parsed {0} items'.format(wiki_handler.items_saved))
+            except PageLimitException as page_limit_exception:
+                LOG.info(page_limit_exception)
+            except KeyboardInterrupt:
+                exit()
+            finally:
+                # A pill for other threads
                 self._q_unparsed_documents.put(None)
-                print(multiprocessing.current_process().pid, c)
-                return
-            page.create_tokens()
-            page.content_clean()
-            if c % 100 == 0:
-                print('progress', multiprocessing.current_process().pid, c)
-            c += 1
 
+    class Parser(multiprocessing.Process):
+        def __init__(self, q_unparsed_documents, p_idf_child):
+            self._q_unparsed_documents = q_unparsed_documents
+            self.p_idf_child = p_idf_child
+            super(self.__class__, self).__init__()
 
-def create_process_parser(**kwargs):
-    processes = []
-    for i in range(0, kwargs['num_of_processes']):
-        process = ProcessParser(kwargs['q_unparsed_documents'])
-        process.start()
-        processes.append(process)
-    return processes
+        def run(self):
+            c = 0
+            while True:
+                page = self._q_unparsed_documents.get()
+                if page is None:
+                    # Just to be sure that other threads can also take a pill
+                    self._q_unparsed_documents.put(None)
+                    self.p_idf_child.send(None)
+                    return
+                page.create_tokens()
+                for token in page.tokens:
+                    self.p_idf_child.send(token.stem)
+                page.content_clean()
+                if c % 100 == 0:
+                    print('progress', multiprocessing.current_process().pid, c)
+                c += 1
+
+    class IDF(multiprocessing.Process):
+        def __init__(self, p_idf_parent):
+            self.p_idf_parent = p_idf_parent
+            self.tokens = {}
+            super(self.__class__, self).__init__()
+
+        def run(self):
+            pills = 0
+            while pills < 4:
+                msg = self.p_idf_parent.recv()
+                if msg is None:
+                    pills += 1
+                    continue
+                if msg in self.tokens.keys():
+                    self.tokens[msg] += 1
+                else:
+                    self.tokens[msg] = 1
+
+    @staticmethod
+    def create_parsers(**kwargs):
+        processes = []
+        for i in range(0, kwargs['num_of_processes']):
+            process = Process.Parser(
+                q_unparsed_documents=kwargs['q_unparsed_documents'],
+                p_idf_child=kwargs['p_idf_child']
+            )
+            processes.append(process)
+        return processes
 
 
 @timer
 def parse():
-    # global session
-    # session = Db.create_session()
     LOG.info("Started loading to database")
 
     q_unparsed_documents = multiprocessing.Queue()
-    p_reader = ProcessReader(q_unparsed_documents)
-    p_reader.start()
+    p_idf_parent, p_idf_child = multiprocessing.Pipe()
+
+    ps_reader = Process.Reader(q_unparsed_documents=q_unparsed_documents)
+    ps_parsers = Process.create_parsers(
+        num_of_processes=int(CONF['general']['processes']),
+        q_unparsed_documents=q_unparsed_documents,
+        p_idf_child=p_idf_child
+    )
+    ps_idf = Process.IDF(p_idf_parent=p_idf_parent)
+
+    ps_reader.start()
 
     LOG.debug('Spawning {0} parser processes'.format(int(CONF['general'][
                                                          'processes'])))
-    ps_parser = create_process_parser(
-        num_of_processes=int(CONF['general']['processes']),
-        q_unparsed_documents=q_unparsed_documents
-    )
+    for ps_parser in ps_parsers:
+        ps_parser.start()
+    ps_idf.start()
 
-    p_reader.join()
-    for p_parser in ps_parser:
-        p_parser.join()
+    ps_reader.join()
+    for ps_parser in ps_parsers:
+        ps_parser.join()
 
 
 if __name__ == '__main__':
