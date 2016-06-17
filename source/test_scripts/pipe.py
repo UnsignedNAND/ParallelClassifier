72d805785f8a99c834983ae99202ec89b4e8bba2
Maciej Szankin <maciej.szankin@intel.com>
2016-06-17 00:05:29 -0500

    WIP pipe demo with multiple processing

---
+++ source/test_scripts/pipe.py
@@ -0,0 +1,53 @@
+import multiprocessing
+import time
+
+
+class Process1(multiprocessing.Process):
+    def __init__(self, pipes):
+        self.pipes = pipes
+        super(self.__class__, self).__init__()
+
+    def run(self):
+        print('Process1 start')
+        for i in range(0, 10):
+            for pipe in pipes:
+                pipe.send(i)
+            print('Process1 send', i)
+            time.sleep(1)
+
+
+class Process2(multiprocessing.Process):
+    def __init__(self, pipe):
+        self.pipe = pipe
+        super(self.__class__, self).__init__()
+
+    def run(self):
+        print('Process2 start')
+        while True:
+            val = self.pipe.recv()
+            print(self.pid, 'received', val)
+            if val == 9:
+                break
+
+
+if __name__ == '__main__':
+    processes = 2
+    pipes = []
+    for pid in range(processes):
+        pipe_parent, pipe_child = multiprocessing.Pipe()
+        pipes.append((pipe_parent, pipe_child))
+
+    p1 = Process1([pipe_parent for (pipe_parent, pipe_child) in pipes])
+
+    ps = []
+    for (pipe_parent, pipe_child) in pipes:
+        ps.append(Process2(pipe_child))
+
+    p1.start()
+    for p in ps:
+        p.stat()
+
+    p1.join()
+    p1.start()
+    for p in ps:
+        p.join()
