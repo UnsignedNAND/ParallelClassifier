d0606c29089142ab77fc41d61cb745840cd72b6d
Maciej Szankin <maciej.szankin@intel.com>
2016-06-17 00:04:42 -0500

    added IDF field to tokens

--- source/models/token.py
+++ source/models/token.py
@@ -3,6 +3,8 @@ class Token(object):
     page_id = None
     count = 0
     freq = 0
+    idf = 1.0  # because of formula, even if word does not exist in corpus it
+    #  should have 1.0 value
 
     def __str__(self):
         return '{0:14} {1:4} {2:7}'.format(self.stem, self.count, self.freq)
